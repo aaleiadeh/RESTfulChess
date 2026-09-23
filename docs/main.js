28e174396ef7c9232c64a19957effe51780c3dc2
Ahmad Aleiadeh <88945767+aaleiadeh@users.noreply.github.com>
2026-09-23 14:22:19 -0400

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -17,7 +17,7 @@ let id = params.get("id");
 let isPlayerTwo = id === null ? false : true;
 
 let local = "http://localhost:443";
-let live = "https://https://pieros.com";
+let live = "https://pieros.com";
 let server = live;
 
 const startbtn = document.querySelector("#createbtn");
