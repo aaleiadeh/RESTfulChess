4cfb607f4a8309aacc791af43aebd06f93e34490
Ahmad Aleiadeh <88945767+aaleiadeh@users.noreply.github.com>
2026-09-23 14:57:05 -0400

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -17,7 +17,7 @@ let id = params.get("id");
 let isPlayerTwo = id === null ? false : true;
 
 let local = "http://localhost:443";
-let live = "https://app.pieros.dev";
+let live = " https://outdoors-granny-jamie-tvs.trycloudflare.com";
 let server = live;
 
 const startbtn = document.querySelector("#createbtn");
