5654e4be5799140beca5e519636441c80ea1d91a
Ahmad Aleiadeh <aleiadehahmad@gmail.com>
2026-09-23 14:44:34 -0400

    updated cors

--- ChessServer/src/main/java/chess/BoardController.java
+++ ChessServer/src/main/java/chess/BoardController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins={"https://aaleiadeh.github.io/","https://app.pieros.dev"})
+@CrossOrigin(origins={"https://aaleiadeh.github.io","https://app.pieros.dev"})
 public class BoardController {
 	public Hashtable<Integer, Board> gameTable = new Hashtable<Integer, Board>();
 	public int id = 0;
